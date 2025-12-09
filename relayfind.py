eab7855ad24a1ff5c308d55782541ce562381229
Igor Molchanov <akemi_homura@kurisa.ch>
2025-12-09 22:15:49 +0300

    Make relayfind search for all relay boards instead of the first one

--- relayfind.py
+++ relayfind.py
@@ -8,7 +8,7 @@ commands = list(map(lambda x: str(x)+'+', relays)) + list(map(lambda x: str(x)+'
 parser.add_argument('-b', '--bind', default='192.168.1.100', type=str, metavar='IPADDR', help='IP address of the local interface to search on (default: %(default)s)')
 parser.add_argument('-p', '--port', default=60000, type=int, metavar='PORT', help='TCP port to send detection packet from (default: %(default)s)')
 parser.add_argument('-P', '--relayport', default=50000, type=int, metavar='PORT', help='TCP port to send detection packet to (default: %(default)s)')
-parser.add_argument('-t', '--timeout', default=10, type=int, metavar='SEC', help='seconds to wait for relay board to reply (default: %(default)s)')
+parser.add_argument('-t', '--timeout', default=3, type=int, metavar='SEC', help='seconds to wait for relay board to reply (default: %(default)s)')
 parser.add_argument('-e', '--encoding', default='utf-8', type=str, metavar='ENCODING', help='encoding to read module name in (default: %(default)s)')
 parser.add_argument('-j', '--json', action='store_true', help='output data in JSON instead of human-readable format (json module shall be available)')
 parser.add_argument('-v', '--verbose', action='store_true', help='be verbose: tell about what is being done')
@@ -24,25 +24,28 @@ if args.verbose: print(f'Waiting for a reply for {args.timeout} seconds...')
 
 sock.settimeout(args.timeout)
 if args.json: import json
+found = []
 try:
-    data, addr = sock.recvfrom(1024)
-    if args.verbose: print(f'Reply is received from {addr[0]}:{addr[1]}')
-    if data[0:16] != b'CH9120_CFG_FLAG\0':
-        if args.verbose: print(f'Wrong payload data:', data[0:16].hex(' '))
-        if args.json: print(json.dumps({"error": errno.EINVAL}))
-        exit(errno.EINVAL)
-    namelen = int(data[29])
-    mac, ip, name, version = data[17:23].hex(':'), '.'.join(f'{c}' for c in data[30:34]), data[34:29+namelen].decode(args.encoding), int(data[30+namelen])
-    if args.json:
-        import json
-        print(json.dumps({"error": 0, "mac": mac, "ip": ip, "name": name, "version": version}))
-    else:
-        print('  MAC address:     ', mac)
-        print('  IP Address:      ', ip)
-        print('  Module name:     ', name)
-        print('  Firmware vesrion:', version)
-    exit(0)
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if args.verbose: print(f'Reply is received from {addr[0]}:{addr[1]}')
+        if data[0:16] != b'CH9120_CFG_FLAG\0':
+            if args.verbose: print(f'Wrong payload data:', data[0:16].hex(' '))
+            if args.json: print(json.dumps({"error": errno.EINVAL}))
+            exit(errno.EINVAL)
+        namelen = int(data[29])
+        mac, ip, name, version = data[17:23].hex(':'), '.'.join(f'{c}' for c in data[30:34]), data[34:29+namelen].decode(args.encoding), int(data[30+namelen])
+        found.append({"mac": mac, "ip": ip, "name": name, "version": version})
+        if not args.json:
+            print('Found a relay:')
+            print('  MAC address:     ', mac)
+            print('  IP Address:      ', ip)
+            print('  Module name:     ', name)
+            print('  Firmware vesrion:', version)
 except socket.timeout:
+    if found:
+        if args.json: print(json.dumps({"error": 0, "devices": found}))
+        exit(0)
     if args.verbose: print('Relay did not reply for the required period.')
     if args.json: print(json.dumps({"error": errno.ETIMEDOUT}))
     exit(errno.ETIMEDOUT)
